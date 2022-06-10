5c77ae32d9e062a6bd5c495a3eba83be804ca228
koki-sasaki <haiku130819@gmail.com>
2022-06-10 07:06:24 +0000

    temp2を追加

--- main1.go
+++ main1.go
@@ -13,9 +13,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func handler2(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("template2.html")
+	t.Execute(w, nil)
+}
+
 func main() {
 	fmt.Println("サーバースタート([Ctrl] + [C]で終了)")
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/temp", handler2)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
