6bdcf5d25b7bfe53973eba75f95f35c2bf19c1c0
koki-sasaki <haiku130819@gmail.com>
2022-06-22 07:27:10 +0000

    hhh

--- application.go
+++ application.go
@@ -2,69 +2,135 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"text/template"
+
+	//"text/template"
 
 	_ "github.com/lib/pq"
 )
 
-//parsefiles 中身を解読
-// func index(w http.ResponseWriter, r *http.Request) {
-// 	t, _ := template.ParseFiles("template.html")
-// 	list := []string{"Apple", "Banana", "Crocodile"}
+// func result(w http.ResponseWriter, r *http.Request) {
+// 	r.ParseForm()
+// 	msg := r.FormValue("msg")
+// 	t, _ := template.ParseFiles("template2.html")
 // 	//データの受け渡し
-// 	t.Execute(w, list)
+// 	t.Execute(w, msg)
 // }
 
-func form(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("template.html")
-	//データの受け渡し
-	t.Execute(w, nil)
-}
-
-func result(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	msg := r.FormValue("msg")
-	t, _ := template.ParseFiles("template2.html")
-	//データの受け渡し
-	t.Execute(w, msg)
-}
-
 func main() {
-	server := http.Server{
-		Addr: "localhost:8080",
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/delete", delete)
+	http.HandleFunc("/", dbtest)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5432"
+	}
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	http.HandleFunc("/form", form)
-	http.HandleFunc("/result", result)
-	http.HandleFunc("/", dbtest)
-	server.ListenAndServe()
 }
 
 type User struct {
-	ID   string
+	ID   int
 	Name string
 }
 
+type UserList []User
+
 func dbtest(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("postgers", "user = postgres password = posflam dbname = dbtest host = localhost")
+	db, err := sql.Open("postgres", "user = lbfjmvpuhlkgqt password = 57d0c849762cceda81669b7a700fd1fd42ed0e9038d07c612efe60cba4c76d41  dbname = d8ro88e9kahr66 host = ec2-34-225-159-178.compute-1.amazonaws.com sslmode=disable")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close() // 関数がリターンする直前に呼び出される
 
-	rows, err := db.Query("SELECT * FROM test")
+	var ul UserList
+
+	rows, err := db.Query("SELECT * FROM testtable")
 	if err != nil {
 		panic(err.Error())
 	}
 
+	defer rows.Close()
+
 	//rowsのNextが入っているか
-	var u User
+
 	for rows.Next() {
+		var u User
 		rows.Scan(&u.ID, &u.Name)
-		fmt.Fprintln(w, u.ID, u.Name)
+		ul = append(ul, u)
 	}
 
-	defer rows.Close()
+	t, _ := template.ParseFiles("template3.html")
+	t.Execute(w, ul)
+
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", "user = lbfjmvpuhlkgqt password = 57d0c849762cceda81669b7a700fd1fd42ed0e9038d07c612efe60cba4c76d41  dbname = d8ro88e9kahr66 host = ec2-34-225-159-178.compute-1.amazonaws.com sslmode=disable")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close() // 関数がリターンする直前に呼び出される
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS testtable ( id integer, name varchar(32) )")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("INSERT INTO testtable (id, name) VALUES (1, 'flamingo')")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// rows, err := db.Query("SELECT * FROM testtable")
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// //rowsのNextが入っているか
+	// var u User
+	// for rows.Next() {
+	// 	rows.Scan(&u.ID, &u.Name)
+	// 	fmt.Fprintln(w, u.ID, u.Name)
+	// }
+
+	// defer rows.Close()
+	t, _ := template.ParseFiles("template.html")
+	t.Execute(w, nil)
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+
+	db, err := sql.Open("postgres", "user = lbfjmvpuhlkgqt password = 57d0c849762cceda81669b7a700fd1fd42ed0e9038d07c612efe60cba4c76d41  dbname = d8ro88e9kahr66 host = ec2-34-225-159-178.compute-1.amazonaws.com sslmode=disable")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close() // 関数がリターンする直前に呼び出される
+
+	_, err = db.Exec("DROP TABLE testtable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, _ := template.ParseFiles("template2.html")
+	t.Execute(w, nil)
+
+	// rows, err := db.Query("SELECT * FROM testtable")
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// var u User
+	// for rows.Next() {
+	// 	rows.Scan(&u.ID, &u.Name)
+	// 	fmt.Fprintln(w, u.ID, u.Name)
+	// }
+
+	// defer rows.Close()
+	//"user = postgres password = posflam  dbname = test host = localhost sslmode=disable"
 }
